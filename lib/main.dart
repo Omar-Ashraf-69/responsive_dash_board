bddfa2ec2bb0937f6d05b5ce23d4b0ff22f31a9c
OmarAsharf62 <omarbondok62@gmail.com>
2024-04-26 10:53:12 +0300

    Add new dashboard view and adaptive layout widget
    Introduce a new dashboard view with adaptive layout widget for different screen
    sizes. Include desktop layout for larger screens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board_project/views/dash_board_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Responsive Dash Board',
-      theme: ThemeData(
-      useMaterial3: true,
-      ),
+      home: DashBoardView(),
     );
   }
 }

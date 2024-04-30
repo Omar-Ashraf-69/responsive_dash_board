f8b163828051b113300c0dab908007fca11c79c6
OmarAsharf62 <omarbondok62@gmail.com>
2024-04-30 17:56:24 +0300

    edited the spaces between the sections of the secreen and finished the card item

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_dash_board_project/utils/app_images.dart';
+import 'package:responsive_dash_board_project/utils/app_styles.dart';
 
 class CardWidget extends StatelessWidget {
   const CardWidget({super.key});
@@ -18,6 +20,41 @@ class CardWidget extends StatelessWidget {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12),
             ),
+          ),
+          padding: const EdgeInsets.only(
+            right: 20,
+            bottom: 24,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                contentPadding: const EdgeInsets.only(left: 24, right: 18),
+                title: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Text(
+                    'Card Name',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                titleTextStyle: AppStyles.styleMedium16(context),
+                subtitle: const Text('Omar Ashraf'),
+                subtitleTextStyle: AppStyles.styleMedium20(context),
+                trailing: SvgPicture.asset(Assets.imagesGallery),
+              ),
+              const Spacer(),
+              Text(
+                "0918 8124 6521 9531",
+                style: AppStyles.styleSemiBold24(context)
+                    .copyWith(color: Colors.white),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "12/20 - 124",
+                style: AppStyles.styleMedium16(context)
+                    .copyWith(color: Colors.white),
+              ),
+            ],
           ),
         ),
       ),
--- lib/widgets/desktop_layout.dart
+++ lib/widgets/desktop_layout.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board_project/widgets/all_expenses_invoice_section.dart';
 import 'package:responsive_dash_board_project/widgets/card.dart';
 import 'package:responsive_dash_board_project/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board_project/widgets/quick_invoice.dart';
-
-import 'all_expensises.dart';
 
 class DesktopLayout extends StatelessWidget {
   const DesktopLayout({super.key});
@@ -25,7 +22,7 @@ class DesktopLayout extends StatelessWidget {
             ),
           ),
           SizedBox(
-            width: 32,
+            width: 24,
           ),
           Expanded(
             child: CardWidget(),
